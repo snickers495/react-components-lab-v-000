0580e133c2616bd963c6b4a7a525df726bdaddce
Lisa Lee <snickers495@gmail.com>
2018-02-05 14:29:23 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -30,7 +30,14 @@ export class InFrontOfYou extends React.Component {
     )
   )
 };
-export class ButcherShop extends React.Component {};
+export class ButcherShop extends React.Component {render(
+  return React.createElement('div', {className: 'butcher-shop'},
+    React.createElement('p', {}, 'Hello! We have the following products for sale today:'),
+    React.createElement('ul', {}, BUTCHER_PRODUCTS.map(product => {
+      React.createElement('li', {}, product)
+    }))
+  )
+)};
 
 ReactDOM.render(
   React.createElement('div', {}, [
