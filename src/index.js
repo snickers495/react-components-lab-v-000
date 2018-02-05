76ba5f132c77740d73839f888de57ab451339b95
Lisa Lee <snickers495@gmail.com>
2018-02-05 14:19:53 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 
-class OlderCoaster extends React.C
+class OlderCoaster extends React.Component 
 
 const BUTCHER_PRODUCTS = [
   'Tenderloin',
