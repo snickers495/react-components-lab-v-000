fd6db5cd9f32177484e0cc9997b78580d8db282c
Lisa Lee <snickers495@gmail.com>
2018-02-05 14:21:40 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ class OlderCoaster extends React.Component {
   render(
     return React.createElement('div', {className="oldercoaster"},
       React.createElement('p', {}, 'Two grannies having the time of their life!'),
-      React.createElement
+      React.createElement('p')
 
     )
   )
