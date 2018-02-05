5d883bffc3be386aff8a1b1995e57066ed13b704
Lisa Lee <snickers495@gmail.com>
2018-02-05 14:24:12 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 class OlderCoaster extends React.Component {
   render(
-    return React.createElement('div', {className="oldercoaster"},
+    return React.createElement('div', {},
       React.createElement('p', {}, 'Two grannies having the time of their life!'),
       React.createElement('p', {}, 'Passengers:'),
       React.createElement('ul', {}, [
